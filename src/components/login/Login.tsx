fa1fc3d7fc59c31e8eb4e30aa669c17c5ad4bbb8
John <johndeveloperxx@gmail.com>
2023-02-09 21:36:06 -0300

    Persistent ball in local storage.

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -4,29 +4,40 @@ import Button from "../Button";
 import { AuthContext } from "../../contexts/authContext";
 const Login = () => {
   const {theme, setTheme} :any  = useContext(AuthContext);
-  const [ballAnim, setBallAnim] = useState<Boolean>(false);
+  const [ballAnim, setBallAnim] = useState <Boolean>(false);
   const anim = () => {
     setBallAnim(!ballAnim);
     themeFunc();
   }
-  
+
   const body = document.body;
   useEffect(() => {
     const sendStorage = localStorage.setItem('theme', JSON.stringify(theme));
     body.classList.toggle('theme');
   }, [theme]);
   const getStorage = localStorage.getItem('theme');
+  const sendStorage = localStorage.setItem('ball', JSON.stringify(ballAnim));
+  const getStorageBall = localStorage.getItem('ball');
   
+  useEffect(()=>{
+    switch(getStorage){
+      case 'true':
+        setBallAnim(true);
+      break;
+      case 'false':
+        setBallAnim(false);
+  };[window.onload]});
+
   useEffect(() => {
     switch(getStorage){
       case 'true':
-      setTheme(true);
+        setTheme(true);
       break;
       case 'false':
         setTheme(false);
     }
   },[window.onload])
-  
+
   const themeFunc = () => {
     setTheme(!theme); 
   }
@@ -34,9 +45,11 @@ const Login = () => {
   const handleForm = (e: any) => e.preventDefault();
   return (
     <div>
+
       <button onClick={anim} className="rounded-3xl bg-violet-900 absolute w-16 right-2 top-2">
          <div className={!ballAnim ? "bg-white duration-500 rounded-full w-5 h-5 relative left-0" : "duration-500 rounded-full w-5 h-5 relative left-13 bg-black"}></div>
       </button>
+
       <div className={!theme ? "bg-white shadow-xl md:w-80 mx-auto mt-10 p-2 h-72 rounded-xl" : "shadow-xl md:w-80 mx-auto mt-10 p-2 h-72 rounded-xl bg-slate-900"}>
           <form
           onSubmit={handleForm}
