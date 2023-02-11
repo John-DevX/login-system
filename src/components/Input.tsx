63b8feb9f8e3308b4e2a0bdf666cba6e275c47fb
John <johndeveloperxx@gmail.com>
2023-02-11 05:10:52 -0300

    Form refactoring in black theme.

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,12 @@
+import {useContext} from 'react';
+import { ThemeContext } from '../contexts/themeContext';
+
 const Input = ({placeholder, type, onChange} : {placeholder: string, type: string, onChange: any}) :JSX.Element => {
+  const {theme} :any = useContext(ThemeContext);
 return (
     <>
      <input 
-     className="border-2 border-slate-300 outline-none focus:border-indigo-600 mb-2 rounded focus:bg-slate-100 p-1"
+     className={theme ? "bg-black border-2 border-indigo-900 outline-none focus:border-indigo-600 mb-2 rounded focus:bg-slate-900 p-1" : "border-2 border-slate-300 outline-none focus:border-indigo-600 mb-2 rounded focus:bg-slate-100 p-1"}
      type={type}
      placeholder={placeholder}
      onChange={onChange}
