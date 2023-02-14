10b61da30a6435b37a8afac211c2d71a2ff193ff
John <johndeveloperxx@gmail.com>
2023-02-14 18:20:04 -0300

    Edited field.

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -13,7 +13,7 @@ const Input = ({placeholder, type, register, name} : typeProp)  :JSX.Element => 
 return (
     <div>
      <input 
-     className={theme ? "bg-black border-2 border-indigo-900 outline-none focus:border-indigo-600 mb-2 rounded focus:bg-slate-900 p-2 relative" : "border-2 border-slate-300 outline-none focus:border-indigo-600 mb-2 rounded focus:bg-slate-100 p-2 relative"}
+     className={theme ? "bg-black border-2 border-indigo-900 outline-none focus:border-indigo-600 mb-2 rounded focus:bg-slate-900 p-2 w-52 relative" : "border-2 border-zinc-300 outline-none focus:border-indigo-700 mb-2 rounded focus:bg-slate-100 p-2 relative w-52"}
      type={type}
      name={name}
      placeholder={placeholder}
